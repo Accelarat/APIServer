3163dff4e8aa6076a3960964596623dfb7cb7ef8
Djalah Ihlasov <dikhlasov@mail.ru>
2023-02-16 08:41:04 +0300

    small fix for entity

--- src/main/java/com/ihlasov/apiserver/entity/User.java
+++ src/main/java/com/ihlasov/apiserver/entity/User.java
@@ -2,9 +2,7 @@ package com.ihlasov.apiserver.entity;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
@@ -16,6 +14,7 @@ import javax.persistence.Table;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     String uri;
     String name;
