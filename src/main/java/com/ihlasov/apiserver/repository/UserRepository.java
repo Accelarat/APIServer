0862f3c823a2398cb724918153fec7204be8cfc8
Djalah Ihlasov <dikhlasov@mail.ru>
2023-02-16 08:19:48 +0300

    jpaRepository added

---
+++ src/main/java/com/ihlasov/apiserver/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ihlasov.apiserver.repository;
+
+import com.ihlasov.apiserver.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
