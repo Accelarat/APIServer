32fa2dce1880e32d0a23d74d1c7f0a9281b13621
Djalah Ihlasov <dikhlasov@mail.ru>
2023-02-17 22:57:53 +0300

    exception handler added

---
+++ src/main/java/com/ihlasov/apiserver/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.ihlasov.apiserver;
+
+
+import com.ihlasov.apiserver.exception.ServiceException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final String DEFAULT_MESSAGE = "Что-то пошло не так :(";
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<String> globalException() {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(DEFAULT_MESSAGE);
+    }
+
+    @ExceptionHandler(ServiceException.class)
+    public ResponseEntity<String> stringResponseEntity(ServiceException exception) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(exception.getMessage());
+    }
+}
