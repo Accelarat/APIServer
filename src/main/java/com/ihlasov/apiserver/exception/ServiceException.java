3adb732b81b44c7061736800375b826d733d8ebc
Djalah Ihlasov <dikhlasov@mail.ru>
2023-02-17 23:00:19 +0300

    exception refactored

--- src/main/java/com/ihlasov/apiserver/exception/ServiceException.java
+++ src/main/java/com/ihlasov/apiserver/exception/ServiceException.java
@@ -1,15 +1,7 @@
 package com.ihlasov.apiserver.exception;
 
 public class ServiceException extends RuntimeException {
-    private final String message;
-
     public ServiceException(String message) {
-        super();
-        this.message = message;
-    }
-
-    @Override
-    public String getMessage() {
-        return message;
+        super(message);
     }
 }
