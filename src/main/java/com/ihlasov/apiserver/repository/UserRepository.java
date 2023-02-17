80c89e54e9128aeca50226d597fae25046686524
Djalah Ihlasov <dikhlasov@mail.ru>
2023-02-17 18:19:04 +0300

    userRepository new method

--- src/main/java/com/ihlasov/apiserver/repository/UserRepository.java
+++ src/main/java/com/ihlasov/apiserver/repository/UserRepository.java
@@ -2,7 +2,15 @@ package com.ihlasov.apiserver.repository;
 
 import com.ihlasov.apiserver.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query(value = "SELECT * from users where status = :status and last_status_change > :time",
+    nativeQuery = true)
+    List<User> findUsersByStatusAndLastStatusChange(String status, LocalDateTime time);
+}
