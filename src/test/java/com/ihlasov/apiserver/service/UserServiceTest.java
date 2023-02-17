cf74ab5e9d44f94c1a94920384a1eae7272ffbf1
Djalah Ihlasov <dikhlasov@mail.ru>
2023-02-18 00:44:14 +0300

    service tests added

--- src/test/java/com/ihlasov/apiserver/service/UserServiceTest.java
+++ src/test/java/com/ihlasov/apiserver/service/UserServiceTest.java
@@ -1,8 +1,10 @@
 package com.ihlasov.apiserver.service;
 
+import com.ihlasov.apiserver.entity.User;
 import com.ihlasov.apiserver.repository.UserRepository;
 import com.ihlasov.apiserver.wrapper.FilesWrapper;
 import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,8 +12,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockMultipartFile;
 
 import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest(classes = UserService.class)
@@ -23,6 +30,27 @@ class UserServiceTest {
     private UserRepository repository;
     @MockBean
     private FilesWrapper filesWrapper;
+
+    Long userId = 1L;
+    String uri = "C:/jpg/fileName";
+    String name = "Peter";
+    String email = "email@ooo.com";
+    String oldStatus = "Offline";
+    String newStatus = "Online";
+    LocalDateTime lastStatusChange = LocalDateTime.of(2023, 2, 18, 0, 6);
+    User user;
+
+    @Test
+    @BeforeEach
+    void setUp() {
+        user = User.builder()
+                .id(userId)
+                .uri(uri)
+                .name(name)
+                .email(email)
+                .status(oldStatus)
+                .lastStatusChange(lastStatusChange).build();
+    }
 
     @SneakyThrows
     @Test
@@ -42,17 +70,46 @@ class UserServiceTest {
 
     @Test
     void createUser() {
+        //when
+        when(repository.save(any(User.class))).thenReturn(user);
+
+        //then
+        assertEquals(1L, service.createUser(uri, name, email));
     }
 
     @Test
     void getUser() {
+        //given
+        long userId = 1L;
+        var user = new User();
+
+        //when
+        when(repository.findById(userId)).thenReturn(Optional.of(user));
+
+        //then
+        assertEquals(Optional.of(user), service.getUser(1L));
     }
 
     @Test
     void changeStatus() {
+        //when
+        when(repository.findById(userId)).thenReturn(Optional.of(user));
+        var serviceResult = service.changeStatus(userId, newStatus);
+
+        //then
+        assertEquals(oldStatus, serviceResult.getOldStatus());
+        assertEquals(newStatus, serviceResult.getNewStatus());
+        verify(repository).save(any());
     }
 
     @Test
     void getStatuses() {
+        //when
+        when(repository.findUsersByStatusAndLastStatusChange(oldStatus, lastStatusChange)).thenReturn(List.of(user));
+        var serviceResult = service.getStatuses(oldStatus, lastStatusChange);
+
+        //then
+        assertEquals(List.of(user), serviceResult.getStatuses());
+        assertEquals(lastStatusChange, serviceResult.getRequestId());
     }
 }
